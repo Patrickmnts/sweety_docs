8c9d2a4041fcf279c8783bc8589b91aadbfc8b36
Patrick Mounts <patrickmnts@gmail.com>
2016-11-04 13:40:53 -0700

    create scope for Readings that were created today.

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -1,4 +1,6 @@
 class Reading < ActiveRecord::Base
   belongs_to :user
   validates_associated :user
+
+  scope :created_today, -> { where('DATE(created_at) = ?', Date.today)  }
 end
--- spec/models/reading_spec.rb
+++ spec/models/reading_spec.rb
@@ -9,4 +9,10 @@ describe "Reading" do
   it 'should be able to create new User' do
     expect(@reading).to be_valid
   end
+
+  it 'should know if it has been created today' do
+    created_yesterday = Reading.create(created_at: DateTime.yesterday)
+    expect(Reading.created_today).to include(@reading)
+    expect(Reading.created_today).to_not include(created_yesterday)
+  end
 end
