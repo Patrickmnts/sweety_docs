7b8a2243f27a210122e96c036c08ec15e5acca15
Patrick Mounts <patrickmnts@gmail.com>
2016-11-04 14:31:17 -0700

    specs, templates and partial for ensuring new reading form is only displayed if user is below daily max.

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -1,6 +1,13 @@
 class ReadingsController < ApplicationController
+  before_action :set_user
+  
   def index
     readings = User.find(params[:user_id]).readings
     @readings = readings.created_today
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:user_id])
   end
 end
--- spec/features/request_spec.rb
+++ spec/features/request_spec.rb
@@ -19,4 +19,30 @@ RSpec.describe "Request Specs", :type => :feature do
 
     expect(page).to have_content(@reading.value)
   end
+
+  it 'should not show a users readings from yesterday' do
+    new_reading = @user.readings.create(value: 213, created_at: DateTime.now - 1.day)
+    visit '/'
+    click_link "Test User"
+
+    expect(page).to_not have_content(new_reading.value)
+  end
+
+  it 'should allow a user to see the new reading form if they are under todays limit' do
+    visit '/'
+    click_link "Test User"
+
+    expect(page).to have_content('Submit new reading')
+  end
+
+  it 'should not allow a user to see new reading form if they are at the daily max' do
+    3.times do
+      @user.readings.create(value: rand(500))
+    end
+
+    visit '/'
+    click_link "Test User"
+
+    expect(page).to_not have_content('Submit new reading')
+  end
 end
