a049e8bfa4aa9d41b2d98431672e2228cb46982b
Patrick Mounts <patrickmnts@gmail.com>
2016-11-04 15:11:10 -0700

    remove shallow tag from nested route.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#index'
+  # resources :readings
   resources :users do
-    resources :readings, shallow: true
+    resources :readings
   end
 end
