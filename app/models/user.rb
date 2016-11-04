70b5a8d23d2cf854680facd95f546208fe69aec7
Patrick Mounts <patrickmnts@gmail.com>
2016-11-04 13:44:44 -0700

    validate user can only create four readings per day

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,19 @@
 class User < ActiveRecord::Base
   has_many :readings
+
+  validate :less_than_four_readings_today
   validates_presence_of :first_name, :last_name
+
+  def readings_today
+    readings.created_today.length
+  end
+
+  private
+  def less_than_four_readings_today
+    if readings_today > 4
+      errors.add :readings, "You may only submit four readings today."
+    end
+  end
+
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,26 @@ describe "User" do
   it 'should be able to create new User' do
     expect(@user).to be_valid
   end
+
+  it 'should be able to have many readings' do
+    reading1 = @user.readings.create
+    reading2 = @user.readings.create
+
+    expect(@user.readings.length).to eq(2)
+    expect(@user.readings).to include(reading1)
+    expect(@user.readings).to include(reading2)
+  end
+
+  it 'should make sure only 4 readings are taken each day' do
+    4.times do
+      @user.readings.create
+    end
+
+    expect(@user.valid?).to eq(true)
+
+    @user.readings.create
+
+    expect(@user.valid?).to eq(false)
+    expect(@user.errors.messages[:readings]).to include("You may only submit four readings today.")
+  end
 end
