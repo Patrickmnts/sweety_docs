731178ef88dcafa8f36257bec68848abf85bd1ea
Patrick Mounts <patrickmnts@gmail.com>
2016-11-07 19:40:00 -0800

    add Faker to gemfile and create seeds.rb file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(first_name: 'Test', last_name: 'User')
+
+user = User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+puts "Generating Data for #{user.full_name}"
+yesterday = Date.today - 1.day
+one_month_past = yesterday - 1.month
+one_month_past.upto yesterday do |date|
+  4.times do
+    user.readings.create(value: rand(300), created_at: date)
+  end
+end
+puts 'All Done!'
