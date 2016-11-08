ee6f810fbe796efe2c9c1e766c4125b303481627
Patrick Mounts <patrickmnts@gmail.com>
2016-11-07 18:38:10 -0800

    create feature spec for report redirect and expected defaults.

--- spec/features/report_spec.rb
+++ spec/features/report_spec.rb
@@ -13,4 +13,14 @@ RSpec.describe "Report Specs", :type => :feature do
   it "should be able to navgate reports page" do
     expect(page).to have_content("Run a report for #{@user.full_name}")
   end
+
+  it 'should take a user to the results page after running a report' do
+    click_on('Run Report')
+    expect(page).to have_content('Report Results')
+  end
+
+  it 'should default the form to a report for today' do
+    expect(find_field('report_report_type').value).to eq('daily')
+    expect(find_field('report_end_date').value).to eq(Date.today.strftime("%Y-%m-%d"))
+  end
 end
