269672315c69c402c11f198db783180ee6a41884
Patrick Mounts <patrickmnts@gmail.com>
2016-11-07 17:44:18 -0800

    fix timezone issue with readings by converting to utc in created_today queue.

--- app/models/reading.rb
+++ app/models/reading.rb
@@ -3,5 +3,6 @@ class Reading < ActiveRecord::Base
   validates_associated :user
   validates_presence_of :value
 
-  scope :created_today, -> { where('DATE(created_at) = ?', Date.today)  }
+  # Because we are targeting UTC set created_at time we need to run the conversion to UTC.
+  scope :created_today, -> { where('DATE(created_at) = ?', DateTime.now.utc.to_date)  }
 end
