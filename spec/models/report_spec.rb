8423f8875fe42589692176941b59e26c90c1f031
Patrick Mounts <patrickmnts@gmail.com>
2016-11-07 18:07:10 -0800

    model specs for daily reports.

--- spec/models/report_spec.rb
+++ spec/models/report_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 describe "Report" do
   before(:each) do
     @user = User.create(first_name: "Test", last_name: "User")
-    @reading = @user.readings.create(value: rand(300))
+    @reading1 = @user.readings.create(value: 300)
+    @reading2 = @user.readings.create(value: 100)
   end
 
   it 'should build a valid model' do
@@ -12,12 +13,36 @@ describe "Report" do
   end
 
  describe 'daily report' do
+   before(:each) do
+     todays_date = DateTime.now.utc.to_date
+     @report = @user.reports.create(report_type: 'daily', end_date: todays_date)
+     @report.generate
+   end
+
    it 'should only pull readings from specified day' do
      incorrect = @user.readings.create(value: rand(300), created_at: Date.today - 1.day)
-     report = @user.reports.create(report_type: 'daily', end_date: DateTime.now.utc.to_date).generate
+     @report.generate
 
-    #  byebug
-     expect(report.get_readings_for_report).to include(@reading)
+     expect(@report.get_readings_for_report).to include(@reading1)
+     expect(@report.get_readings_for_report).to_not include(incorrect)
+   end
+
+   it 'should calculate the maximum' do
+     expect(@report.maximum).to eq(300)
+   end
+
+   it 'should set the minimum' do
+     expect(@report.minimum).to eq(100)
+   end
+
+   it 'should set the average' do
+     expect(@report.average).to eq(200)
+   end
+
+   it 'should have the same start_date as end_date' do
+     expect(@report.start_date).to eq(@report.end_date)
    end
  end
+
+
 end
